933d901840ce0ddb28a0fabcfbbee178b08ab39d
unknown <geolog197@mail.ru>
2018-01-28 00:27:11 +0300

    Fourth

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var FONT_HEIGHT = 16;
@@ -6,9 +8,9 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var BAR_GAP = 40;
 var BAR_WIDTH = 40;
-var BAR_HEIGHT = - (CLOUD_HEIGHT - 2 * BAR_GAP - 2 * FONT_HEIGHT - GAP);
+var BAR_HEIGHT = -(CLOUD_HEIGHT - 2 * BAR_GAP - 2 * FONT_HEIGHT - GAP);
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
@@ -48,7 +50,7 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - 2 * GAP - FONT_HEIGHT - ((BAR_HEIGHT * times[i]) / maxTime)  * (-1));
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - 2 * GAP - FONT_HEIGHT - ((BAR_HEIGHT * times[i]) / maxTime) * (-1));
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
 
